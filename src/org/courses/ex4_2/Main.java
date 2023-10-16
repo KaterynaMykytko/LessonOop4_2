2d7832770a2ee93914336cd90d522f2ca70554b2
KaterynaMykytko <ekaterina.kaliazina@gmail.com>
2023-10-16 11:53:36 +0200

    Changes in code

--- src/org/courses/ex4_2/Main.java
+++ src/org/courses/ex4_2/Main.java
@@ -6,39 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DOCHandler docHandler = new DOCHandler();
-        TXTHandler txtHandler = new TXTHandler();
-        XMLHandler xmlHandler = new XMLHandler();
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter document's name with format:");
-        String name  = scanner.next();
+        String name = scanner.next();
 
-        switch (name){
-            case ".xml":{
-                xmlHandler.create();
-                xmlHandler.open();
-                xmlHandler.change();
-                xmlHandler.save();
+        AbstractHandler documentHandler = null;
+
+        switch (name) {
+            case ".xml": {
+                documentHandler = new XMLHandler();
                 break;
             }
-            case ".txt":{
-                txtHandler.create();
-                txtHandler.open();
-                txtHandler.change();
-                txtHandler.save();
+            case ".txt": {
+                documentHandler = new TXTHandler();
                 break;
             }
-            case ".doc":{
-                docHandler.create();
-                docHandler.open();
-                docHandler.change();
-                docHandler.save();
+            case ".doc": {
+                documentHandler = new DOCHandler();
                 break;
             }
             default:
                 System.out.println("Incorrect format");
                 break;
+        }
+        if (documentHandler != null) {
+            documentHandler.create();
+            documentHandler.open();
+            documentHandler.change();
+            documentHandler.save();
         }
     }
 }
